7762955f661960f6c03d2955170ccb3d01a94626
rmsharp <rmsharp@me.com>
2017-11-01 13:10:56 -0500

    Added documentation for get_deps()

--- R/rmsutilityr.R
+++ R/rmsutilityr.R
@@ -1,8 +1,28 @@
-#' Has the sideeffect of writing out a LaTeX title page compatible with memoir.
+#' Get Package Dependencies
+#' 
+#' @return Character vector of package dependencies based on the contents of
+#' the \code{DESCRIPTION} file.
+#' 
 #'
 #' Written by Josh O'Brien on stackoverflow on May 13 '15 at 21:42
 #' @param path character vector of length one having the directory path to
-#' where new version of the custom package exists.
+#' where new version of the custom package exists. This can be the 
+#' full path name of the directory in which the package exists or the full
+#' path name of the source version of the package.
+#' @examples 
+#' \dontrun{
+#' library(rmsutilityr)
+#' ## Directory where package is placed when installed.
+#' dependencies <- get_deps("c:/R library/snprcspf")
+#' ## Source file of package
+#' dependencies <- get_deps("c:/R data/snprcspf.1.0.1.tar.gz")
+#' remove_these <- c("stats", "tools", "utils", "rmsutilityr", "animalr")
+#' dependencies <- remove_these_str(dependencies, expunge = remove_these)
+#' repo <- "https://cran.revolutionanalytics.com" ## or your mirror of choice
+#' lib_path <- "c:/R Library/"
+#' install.packages(dependencies, type = "binary", repos = repo, lib = lib_path)
+#' }
+
 #' @export
 get_deps <- function(path) {
   dcf <- read.dcf(file.path(path, "DESCRIPTION"))
