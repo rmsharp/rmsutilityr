5f47dcb91d0692723b4cf020801d0d961d2b846e
rmsharp <rmsharp@me.com>
2018-07-21 14:00:26 -0500

    Added ability to handle NA values in str_detect_fixed_all() and str_detect_regex_all(). Added error detection in is_valid_date_str() when the argument is not a character vector

--- R/is_valid_date_str.R
+++ R/is_valid_date_str.R
@@ -9,6 +9,9 @@
 #' descriptor.
 #' @export
 is_valid_date_str <- function(date_str, format = "%d-%m-%Y %H:%M:%S") {
+  if (!is.character(date_str)) {
+    return(rep(FALSE, length(date_str)))
+  }
   result <- as.logical(sapply(date_str, function(s) {
     d <- try(as.Date(s, format = format))
     !(class(d) == "try-error" || is.na(d))
--- R/str_detect_regex_all.R
+++ R/str_detect_regex_all.R
@@ -1,17 +1,23 @@
 #' Returns a logical vector with results of stri_detect() for each pattern in
 #' second parameters character vector.
 #'
-#' @usage str_detect_regex_all(strings, patterns, ...)
+#' @usage str_detect_regex_all(strings, patterns, ignore_na, ...)
 #' @param strings input vector. This must be an atomic vector and will be
 #' coerced to a character vector.
 #' @param patterns patterns to look for, as defined by a POSIX regular
 #' expression. See the \emph{Extended Regular Expressions} section of regex
 #' for details. See fixed, ignore.case and perl for how to use other
 #' types of matching: fixed, case insensitive and perl-compatible.
+#' @param ignore_na if TRUE NA values are trimmed out of \code{strings} and 
+#' \code{patterns} before comparison
 #' @param ... further arguments for stri_detect_regex
 #' @import stringi
 #' @export
-str_detect_regex_all <- function(strings, patterns, ...) {
+str_detect_regex_all <- function(strings, patterns, ignore_na, ...) {
+  if (ignore_na) {
+    strings <- strings[!is.na(strings)]
+    patterns <- patterns[!is.na(patterns)]
+  }
   sapply(patterns, function(pattern) {
     any(stri_detect_regex(strings, pattern, ...))})
 }
