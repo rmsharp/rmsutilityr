7b2a72476d2ac2ee208f5628a612db882f912c68
rmsharp <rmsharp@me.com>
2018-07-21 19:44:07 -0500

    Allow for having Dates

--- R/is_valid_date_str.R
+++ R/is_valid_date_str.R
@@ -10,7 +10,8 @@
 #' @export
 is_valid_date_str <- function(date_str, format = "%d-%m-%Y %H:%M:%S") {
   if (!is.character(date_str)) {
-    return(rep(FALSE, length(date_str)))
+    if (is.numeric(date_str))
+      return(rep(FALSE, length(date_str)))
   }
   result <- as.logical(sapply(date_str, function(s) {
     d <- try(as.Date(s, format = format))
