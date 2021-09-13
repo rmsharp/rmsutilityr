9981654978c372297302126e56a85be8b6f2859a
rmsharp <rmsharp@me.com>
2021-09-12 22:34:47 -0500

    added clearpage.R

---
+++ R/clearpage.R
@@ -0,0 +1,20 @@
+#' clearpage that works for PDF (LaTeX) and HTML output from \code{knitr}
+#' 
+#' Causes the print buffer to be flushed.
+#' 
+#' @return Returns either a LaTeX command, \code{\\clearpage}, if 
+#' \code{is_latex_output()} is \code{TRUE}, an HTML command,
+#' \code{<div style="page-break-before: always;" />} if 
+#' \code{is_html_output()} is \code{TRUE}, and \code{NULL} if 
+#' neither is \code{TRUE}.
+#' 
+#' @examples 
+#' clearbreak()
+#' @importFrom knitr is_latex_output is_html_output
+#' @export
+clearpage <- function() {
+  if (is_latex_output())
+    return("\\clearpage")
+  else if (is_html_output())
+    return('<div style="page-break-before: always;" />')
+}
