6ada55501e2ac15e3ae171805196940fece089b1
R. Mark Sharp <rmsharp@me.com>
2023-08-29 12:40:09 -0500

    added clearpage()

---
+++ R/clearpage.R
@@ -0,0 +1,18 @@
+#' clearpage that works for PDF (LaTeX) and HTML output from \code{knitr}
+#' 
+#' @return Returns either a LaTeX command, \code{\\clearpage}, if 
+#' \code{is_latex_output()} is \code{TRUE}, an HTML command,
+#' \code{<div style="page-break-before: always;" />} if 
+#' \code{is_html_output()} is \code{TRUE}, and \code{NULL} if 
+#' neither is \code{TRUE}.
+#' 
+#' @examples 
+#' clearpage()
+#' @importFrom knitr is_latex_output is_html_output
+#' @export
+clearpage <- function() {
+  if (is_latex_output())
+    return("\\clearpage")
+  else if (is_html_output())
+    return('<div style="page-break-before: always;" />')
+}
